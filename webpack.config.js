5e69604dc5267809bfe5d5b40f90532a03fe7414
vangogh500 <matsudaskai@gmail.com>
2017-07-30 22:40:10 +0700

    boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var webpack = require('webpack')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'index.js',
+    library: 'react-pixi-game',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  target: 'node',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+     minimize: true,
+     compress: false
+    })
+  ]
+}
